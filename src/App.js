c1c115f3c9250eba3e4174cbb25822f1e4e32ddd
parkcheonju <cjswne@gmail.com>
2023-04-13 23:56:12 +0900

    container&css modify commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Sections/Header"
 import Section1 from "./components/Sections/Section1"
 import Section2 from "./components/Sections/Section2"
 import Section3 from "./components/Sections/Section3"
+// import Sectiontest from "./components/Sections/Section3_test"
 import Section4 from "./components/Sections/Section4"
 import Section5 from "./components/Sections/Section5"
 import Footer from "./components/Sections/Footer"
@@ -14,6 +15,7 @@ function App() {
     <Section1 /> 
     <Section2 /> 
     <Section3 /> 
+    {/* <Sectiontest />  */}
     <Section4 /> 
     <Section5 /> 
     <Footer /> 
